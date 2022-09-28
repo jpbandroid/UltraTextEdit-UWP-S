fb5444d1f317b6b7c150eb4c41b3174195e86874
jpb <jakub.baranski.33@gmail.com>
2022-09-28 19:19:21 +0100

    Add project files.

---
+++ UltraTextEdit Pro/MainPage.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UltraTextEdit_Pro
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private async void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a text file.
+            Windows.Storage.Pickers.FileOpenPicker open =
+                new Windows.Storage.Pickers.FileOpenPicker();
+            open.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            open.FileTypeFilter.Add(".rtf");
+
+            Windows.Storage.StorageFile file = await open.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                try
+                {
+                    Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
+                    // Load the file into the Document property of the RichEditBox.
+                    box.Document.LoadFromStream(Windows.UI.Text.TextSetOptions.FormatRtf, randAccStream);
+                }
+                catch (Exception)
+                {
+                    ContentDialog errorDialog = new ContentDialog()
+                    {
+                        Title = "File open error",
+                        Content = "Sorry, file could not be opened",
+                        PrimaryButtonText = "OK"
+                    };
+
+                    await errorDialog.ShowAsync();
+                }
+            }
+        }
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.Storage.Pickers.FileSavePicker savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Rich Text", new List<string>() { ".rtf" });
+
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+
+            Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until we 
+                // finish making changes and call CompleteUpdatesAsync.
+                Windows.Storage.CachedFileManager.DeferUpdates(file);
+                // write to file
+                Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                    await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+
+                box.Document.SaveToStream(Windows.UI.Text.TextGetOptions.FormatRtf, randAccStream);
+
+                // Let Windows know that we're finished changing the file so the 
+                // other app can update the remote version of the file.
+                Windows.Storage.Provider.FileUpdateStatus status = await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+                if (status != Windows.Storage.Provider.FileUpdateStatus.Complete)
+                {
+                    Windows.UI.Popups.MessageDialog errorBox =
+                        new Windows.UI.Popups.MessageDialog("File " + file.Name + " couldn't be saved.");
+                    await errorBox.ShowAsync();
+                }
+            }
+        }
+        private void FindBoxHighlightMatches()
+        {
+            FindBoxRemoveHighlights();
+
+            Color highlightBackgroundColor = (Color)App.Current.Resources["SystemColorHighlightColor"];
+            Color highlightForegroundColor = (Color)App.Current.Resources["SystemColorHighlightTextColor"];
+
+            string textToFind = findBox.Text;
+            if (textToFind != null)
+            {
+                ITextRange searchRange = box.Document.GetRange(0, 0);
+                while (searchRange.FindText(textToFind, TextConstants.MaxUnitCount, FindOptions.None) > 0)
+                {
+                    searchRange.CharacterFormat.BackgroundColor = highlightBackgroundColor;
+                    searchRange.CharacterFormat.ForegroundColor = highlightForegroundColor;
+                }
+            }
+        }
+
+        private void FindBoxRemoveHighlights()
+        {
+            ITextRange documentRange = box.Document.GetRange(0, TextConstants.MaxUnitCount);
+            SolidColorBrush defaultBackground = box.Background as SolidColorBrush;
+            SolidColorBrush defaultForeground = box.Foreground as SolidColorBrush;
+
+            documentRange.CharacterFormat.BackgroundColor = defaultBackground.Color;
+            documentRange.CharacterFormat.ForegroundColor = defaultForeground.Color;
+        }
+
+        private void Editor_GotFocus(object sender, RoutedEventArgs e)
+        {
+            box.Document.GetText(TextGetOptions.UseCrlf, out string currentRawText);
+
+            // reset colors to correct defaults for Focused state
+            ITextRange documentRange = box.Document.GetRange(0, TextConstants.MaxUnitCount);
+            SolidColorBrush background = (SolidColorBrush)App.Current.Resources["TextControlBackgroundFocused"];
+
+            if (background != null)
+            {
+                documentRange.CharacterFormat.BackgroundColor = background.Color;
+            }
+        }
+
+        private void fileClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(BlankPage1));
+        }
+
+        private void inkc(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GamesPage));
+        }
+
+        private void infinitec(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MusicPage));
+        }
+    }
+}
---
+++ UltraTextEdit Pro/SettingsPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UltraTextEdit_Pro
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            this.InitializeComponent();
+            if (ElementSoundPlayer.State == ElementSoundPlayerState.On)
+                _switch.IsOn = true;
+            if (ElementSoundPlayer.SpatialAudioMode == ElementSpatialAudioMode.On && _switch.IsOn == true)
+                check1.IsChecked = true;
+        }
+        private void spatialAudioBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (_switch.IsOn == true)
+            {
+                ElementSoundPlayer.SpatialAudioMode = ElementSpatialAudioMode.On;
+            }
+        }
+
+        private void spatialAudioBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (_switch.IsOn == true)
+            {
+                ElementSoundPlayer.SpatialAudioMode = ElementSpatialAudioMode.Off;
+            }
+        }
+
+        private void soundToggle_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (_switch.IsOn == true)
+            {
+                check1.IsEnabled = true;
+                ElementSoundPlayer.State = ElementSoundPlayerState.On;
+            }
+            else
+            {
+                check1.IsEnabled = false;
+                check1.IsChecked = false;
+
+                ElementSoundPlayer.State = ElementSoundPlayerState.Off;
+                ElementSoundPlayer.SpatialAudioMode = ElementSpatialAudioMode.Off;
+            }
+        }
+    }
+}
